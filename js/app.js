5bc9472aa4a9e3901eb36c06aa8309e18a4fc954
Darko Sedlarevic <sedlarevic.darko@gmail.com>
2019-04-17 20:53:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -158,7 +158,7 @@ caseBackColor.on('change', (newColor) => {
     return;
   }
   var colorString = newColor.toHEX().toString();
-  $('.case-back-color').css('backgroundColor', colorString);
+  //$('.case-back-color').css('backgroundColor', colorString);
   $('.case-back .b, .case-back-serial .l').css("fill", colorString);
 });
 
@@ -202,37 +202,88 @@ const testColor = new Pickr({
         interaction: {
             hex: true,
             rgba: true,
-            hsva: true,
+            hsva: false,
             input: true,
-            clear: true,
+            clear: false,
             save: false
         }
     }
 });
 
-// button click
-$('.color-container').hide();
 
-$('#test').click(function () {
+
+
+// button click
+// $('.color-container').hide();
+
+$('.close-picker-container').click(function () {
 		// $('.color-container').show();
-    $('.color-container').toggle();
+    // $('.color-container').hide();
+    TweenLite.fromTo ($('.color-container') , 0.3, {bottom:80, opacity:1}, {bottom:0, opacity:0, display:'none'})  //animate with TweenLite
 });
 
+// When the user clicks anywhere outside of the color picker, close it
+// https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_modal_close
+// https://gomakethings.com/detecting-clicks-outside-of-an-element-with-vanilla-javascript/
+// var colorPickerContainer = document.querySelector('.pcr-app')
+// window.onclick = function(event) {
+//   if (event.target == colorPickerContainer) {
+//     //colorPickerContainer.style.display = 'none';
+//     //alert("bu")
+//   }
+//   else {
+//     colorPickerContainer.style.display = 'none';
+//     // alert("bu")
+//   }
+// }
+// // Listen for all clicks on the document
+document.addEventListener('click', function (event) {
+
+    // If the click happened inside the the container, bail
+    //if user cliks  li button or color picker container do nothing
+    if (event.target.closest('.color-container, .position-test, .resize-slider-container, .resize-slider')) return;
+
+    // Otherwise, run our code...
+    console.log('outside');
+    TweenLite.fromTo ($('.color-container, .resize-slider-container') , 0.3, {bottom:80, opacity:1}, {bottom:-500, opacity:1, display:'none'})  //animate with TweenLite
+
+
+}, false);
+
+// $('.color-container').on("click", function(event){
+//   // If the click happened inside the the container, bail
+//   if (event.target.closest('.visible')) return;
+//
+//   // Otherwise, run our code...
+//   console.log('outside');
+//   TweenLite.fromTo ($('.color-container') , 0.4, {bottom:80, opacity:1}, {bottom:0, opacity:0, display:'none'})  //animate with TweenLite
+//
+//
+// });
 
 
 
 // test position of element
 // use http://api.jquery.com/width/ to see iz menu width =<> body width
 //and then position the container
-$("#position-test").click(function(){
+//added for each so i can have same class on all buttons
+$('.position-test').each(function(index) {
+    $(this).on("click", function(){
+// $(".position-test").click(function(){
   // MY iDEA:
   // if: 	picker btn position left + picker container width = or < body width
   //    show container top left of the btn
-  // else:  simulate css - left 50% - margin minus 50% of elements width (dinamicly) - so i can set manualy in css difrent width's of container 
+  // else:  simulate css - left 50% - margin minus 50% of elements width (dinamicly) - so i can set manualy in css difrent width's of container
   var bodyWidth = $('body').width();
       colorPickerWidth = $('.color-container').outerWidth();
       colorPickerHalfWidth = -(colorPickerWidth/2);
-      btnPosition = $("#position-test").offset(); // retrieve the current position of an element relative to the document -read jquery docs
+      //btnPosition = $(".position-test").offset(); // retrieve the current position of an element relative to the document -read jquery docs
+      btnPosition = $(this).offset(); // retrieve the current position of  clicked element  relative to the document -read jquery docs
+      colorFor = this.getAttribute("data-picker-for");//get data atribute of clicked button
+      //colorFor = this.getAttribute("data-picker-for");//get data atribute of clicked button
+
+
+
 
   // var menuWidth = $('.controls-menu-ul li').outerWidth();
   //sum all li items from: https://stackoverflow.com/questions/25409750/calculate-width-by-the-sum-of-all-childs
@@ -247,11 +298,15 @@ $("#position-test").click(function(){
 
   if(btnPosition.left + colorPickerWidth <=  bodyWidth) {
     //show container top left of the btn
-    $('.color-container').css({"left" : btnPosition.left, "margin-left" : "10px" }).toggle(); // add margin for li element padding
+    $('.color-container').css({"left" : btnPosition.left, "margin-left" : "10px" }); // add margin for li element padding
+    TweenLite.fromTo ($('.color-container') , 0.3, {bottom:0, opacity:0}, {bottom:80, opacity:1, display:'block'})  //animate with TweenLite
   }
   else {
     // simulate css - left 50% - margin 50% of elements width - so i can change manualy in css difrent width's of container
-    $('.color-container').css({ 'left' : '50%', 'margin-left' : colorPickerHalfWidth }).toggle();
+    // $('.color-container').css({ 'left' : '50%', 'margin-left' : colorPickerHalfWidth }).show();
+    $('.color-container').css({ 'left' : '50%', 'margin-left' : colorPickerHalfWidth });
+    TweenLite.fromTo ($('.color-container') , 0.3, {bottom:0, opacity:0}, {bottom:80, opacity:1, display:'block'})  //animate with TweenLite
+
     // $('.color-container').addClass('color-container-center').toggle();
   }
 
@@ -260,6 +315,46 @@ $("#position-test").click(function(){
   // $('.color-container').css({"left" : x.left, "margin-left" : "10px" }).toggle(); // add margin for li element padding
 
   // alert("Top position: " + x.top + " Left position: " + x.left);
+
+///////////////////////////////////////////////////////////
+// control on change pickr event with data atributes
+console.log('data-atribute: ' + colorFor); // just for testing delete
+
+testColor.on('change', (newColor) => {
+  if (newColor === null) {
+    console.log("unhappy");
+    return;
+  }
+  else if(colorFor == 'wallpaper-background-color') {
+      var colorString = newColor.toHEX().toString();
+      $('.wallpaper-background-color, .screensaver-container').css('backgroundColor', colorString);
+
+  }
+  else if(colorFor == 'case-back-background-color') {
+      var colorString = newColor.toHEX().toString();
+      $('.case-back-background-color, .case-back-container').css('backgroundColor', colorString);
+      //change color of li text
+      // $('.case-back-background-color').css('color', colorString);
+
+  }
+  else if(colorFor == 'case-back-color') {
+      var colorString = newColor.toHEX().toString();
+      // $('.case-back-color').css('backgroundColor', colorString);
+      $('.case-back .b, .case-back-serial .l').css("fill", colorString);
+  }
+  else{
+ // if none of the above are TRUE then do nothing.
+  }
+//--------------------------------------------------------------
+
+
+});
+
+///////////////////////////////////////////////////
+
+
+});
+
 });
 
 
@@ -273,7 +368,35 @@ $("#position-test").click(function(){
 
 //============================================================
 
+//============================================================
+// RANGE SLIDER FOR RESIZING
 
+$('.resize-slider').each(function(index) {
+    $(this).on("click", function(){
+  var bodyWidth = $('body').width();
+      sliderContainerWidth = $('.resize-slider-container').outerWidth();
+      sliderContainerHalfWidth = -(sliderContainerWidth/2);
+      btnPosition = $(this).offset(); // retrieve the current position of  clicked element  relative to the document -read jquery docs
+
+  if(btnPosition.left + sliderContainerWidth <=  bodyWidth) {
+    //show container top left of the btn
+    $('.resize-slider-container').css({"left" : btnPosition.left, "margin-left" : "10px" }); // add margin for li element padding
+    TweenLite.fromTo ($('.resize-slider-container') , 0.3, {bottom:-500, opacity:1}, {bottom:80, opacity:1, display:'block'})  //animate with TweenLite
+  }
+  else {
+    // simulate css - left 50% - margin 50% of elements width - so i can change manualy in css difrent width's of container
+    // $('.color-container').css({ 'left' : '50%', 'margin-left' : colorPickerHalfWidth }).show();
+    $('.resize-slider-container').css({ 'left' : '50%', 'margin-left' : sliderContainerHalfWidth });
+    TweenLite.fromTo ($('.resize-slider-container') , 0.3, {bottom:-500, opacity:1}, {bottom:80, opacity:1, display:'block'})  //animate with TweenLite
+
+    // $('.color-container').addClass('color-container-center').toggle();
+ }
+
+  });
+ });
+
+
+//===========================================================
 
 
 
@@ -297,12 +420,37 @@ var range = document.querySelector("#range");
 var caseBack = document.querySelector(".case-back-container");
 var sizeNum = document.querySelector(".size-num");
 
+
+// plus/ minus range slider input buttons
+// code from: http://jsfiddle.net/kgLsky8s/2/
+//read https://www.w3schools.com/jsref/jsref_parseint.asp
+//read: https://www.w3schools.com/jsref/jsref_parsefloat.asp
+//
+$("#plus").click(function(){
+      var newValuePlus = parseFloat($("#range").val()) * 10 + 1;
+      if ( newValuePlus > 100 ) return;
+      $("#range").val(newValuePlus/10);
+      TweenLite.to(caseBack, 1,{scale: newValuePlus/10});
+      sizeNum.innerText = "x " + newValuePlus/10;
+      console.log(newValuePlus/10)
+  });
+  $("#minus").click(function(){
+      var newValueMinus = parseFloat($("#range").val()) - 0.1;
+      if ( newValueMinus < 0.2 ) return;
+      $("#range").val(newValueMinus);
+      TweenLite.to(caseBack, 1,{scale: newValueMinus});
+      sizeNum.innerText = "x " + newValueMinus;
+      console.log(newValueMinus)
+  });
+
+
 range.addEventListener("input", function() {
   // css scale add new transform
   //caseBack.style.transform = "scale(" + this.value + ")";
   // i need to use greensock TweenLite becouse od draggable
   TweenLite.to(caseBack, 1,{scale: this.value});
   sizeNum.innerText = "x " + this.value;
+  console.log(this.value)
 });
 
 
