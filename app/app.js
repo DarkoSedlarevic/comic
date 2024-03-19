281411a6bf26c3f779b8c609ee2a969d3fdcd6cf
Darko Sedlarevic <sedlarevic.darko@gmail.com>
2024-03-19 16:59:47 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@
 var   app = document.getElementById("app");
         step1 = document.getElementById("step-1");
         appWrap = document.getElementById("app-wrap");
-        uploadVoice = new Audio('voice/hello.mp3?raw=true');
+        uploadVoice = new Audio('https://github.com/DarkoSedlarevic/comic/blob/master/app/voice/hello.MP3');
 
 
            appWrap.classList.add("active");
